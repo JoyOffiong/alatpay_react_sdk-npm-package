a28fff96d7f347211c6f9d142c34650c692b96d5
joy offiong <blaqkly@gmail.com>
2025-04-21 10:45:46 +0100

    new version push

---
+++ dist/useALATPay.js
@@ -0,0 +1,34 @@
+function UseALATPay({ amount, apiKey, businessId, currency, email, firstName, lastName, metadata, phone, onTransaction, onClose }) {
+    const submit = (formData) => {
+        if (!window.Alatpay) {
+            const script = document.createElement("script");
+            script.src = "https://web.alatpay.ng/js/alatpay.js";
+            script.async = true;
+            script.onerror = () => {
+                console.error("Failed to load AlatPay script.");
+            };
+            document.body.appendChild(script);
+            setTimeout(() => { submit(formData); }, 1000);
+            return;
+        }
+        const config = {
+            apiKey,
+            businessId,
+            email,
+            phone,
+            firstName,
+            lastName,
+            amount,
+            currency,
+            metadata,
+            onTransaction,
+            onClose
+        };
+        const newPopup = window.Alatpay.setup(config);
+        newPopup.show();
+    };
+    return {
+        submit
+    };
+}
+export default UseALATPay;
